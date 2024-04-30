7963e7d8c5a1bc58661ca0e15100b89680d4d4ab
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-04-30 15:33:38 +0530

    bug fixes

--- code/weapon.py
+++ code/weapon.py
@@ -17,10 +17,10 @@ class Weapon(pygame.sprite.Sprite):
                 midleft=player.rect.midright + pygame.math.Vector2(0, 5))
         elif direction == 'left':
             self.rect = self.image.get_rect(
-                midright=player.rect.midleft + pygame.math.Vector2(0, 5))
+                midright=player.rect.midleft + pygame.math.Vector2(15, 3))
         elif direction == 'down':
             self.rect = self.image.get_rect(
-                midtop=player.rect.midbottom + pygame.math.Vector2(-10, -30))
+                midtop=player.rect.midbottom + pygame.math.Vector2(-12, -55))
         else:
             self.rect = self.image.get_rect(
-                midbottom=player.rect.midtop + pygame.math.Vector2(30, 30))
+                midbottom=player.rect.midtop + pygame.math.Vector2(-30, 30))
